d7a13f328f281891e4574c8f60a53997e0b30791
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-30 00:04:38 -0700

    Created FastApi entry point

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
 # FastAPI entry point
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.rag.pipeline import rag_pipeline
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query : str
+
+@app.post("/query")
+def query_rag(req : QueryRequest):
+    return rag_pipeline(req.query)
