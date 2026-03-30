f3e9ea0343f46bdd6f0f272d921f5f6ad86a75df
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-29 23:11:52 -0700

    Added generator logic

--- app/rag/generator.py
+++ app/rag/generator.py
@@ -1,1 +1,22 @@
 # LLM calls
+import requests
+from app.config import OLLAMA_BASE_URL, LLM_MODEL
+
+def generate_answer(query : str, context : str):
+    prompt = f"""
+    You are a helpful assistant.
+    
+    Context : {context}
+    Question : {query}
+
+    Answer:
+    """
+    response = requests.post(
+        f"{OLLAMA_BASE_URL}/api/generate",
+        json={
+            "model" : LLM_MODEL,
+            "prompt" : prompt,
+            "stream" : False
+        }
+    )
+    return response.json()["response"]
