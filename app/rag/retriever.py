f244aec457269c7d9f0a212c03b02d5cfb7939d0
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-29 20:23:41 -0700

    Added retriver logic

--- app/rag/retriever.py
+++ app/rag/retriever.py
@@ -1,1 +1,16 @@
 # Vector Search
+import chromadb
+from app.config import CHROMA_DB_DIR, COLLECTION_NAME
+from app.rag.embedder import get_embeddings
+
+# “Persistent” means data is stored on disk and not lost when the program stops.
+client = chromadb.PersistentClient(path=CHROMA_DB_DIR)
+collection = client.get_or_create_collection(name=COLLECTION_NAME)
+
+def retrieve(query : str, k : int = 3):
+    query_embedding = get_embeddings(query)
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=k
+    )
+    return results["documents"][0]
