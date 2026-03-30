69c92cca1cceeffe518e5e5ad3bd7fb3e21251d0
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-30 00:16:01 -0700

    Refactored the code as well as the file structure

--- app/ingestion/loader.py
+++ app/ingestion/loader.py
@@ -5,7 +5,7 @@ import os
 def load_docs(data_path="data/docs"):
     docs = []
     for file in os.listdir(data_path):
-        with open(os.path.join(data_path, file), "r") as f:
+        with open(os.path.join(data_path, file,encoding="utf-8"), "r") as f:
             docs.append(f.read())
     return docs
 
