0f77345c5d68cb2280e072bf0629a314e9f8bad7
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-29 23:31:58 -0700

    Added loader logic

--- app/ingestion/loader.py
+++ app/ingestion/loader.py
@@ -1,1 +1,11 @@
 # Load docs
+
+import os
+
+def load_docs(data_path="data/docs"):
+    docs = []
+    for file in os.listdir(data_path):
+        with open(os.path.join(data_path, file), "r") as f:
+            docs.append(f.read())
+    return docs
+
