85069b6b9677bee8ce3ba20c1c95e301f8c4666e
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-29 23:20:23 -0700

    Added rag  logic

--- app/rag/pipeline.py
+++ app/rag/pipeline.py
@@ -1,1 +1,14 @@
 # Core RAG Logic
+
+from app.rag.retriever import retrieve
+from app.rag.generator import generate_answer
+
+def rag_pipeline(query : str):
+    docs = retrieve(query)
+    context = "\n\n".join(docs)
+    answer = generate_answer(query, context)
+    return {
+        "query" : query,
+        "context" : docs,
+        "answer" : answer
+    }
