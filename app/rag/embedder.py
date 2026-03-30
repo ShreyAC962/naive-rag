b78ba3031e285962398d1955b7a68dbcf32b095b
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-29 20:05:05 -0700

    Added embedder logic

--- app/rag/embedder.py
+++ app/rag/embedder.py
@@ -1,1 +1,11 @@
 # Embedding logic
+import requests
+from app.config import OLLAMA_BASE_URL, EMBED_MODEL
+
+def get_embeddings(text : str):
+    response = requests.post(f"{OLLAMA_BASE_URL}/api/embeddings",
+        json = {
+        "model" : EMBED_MODEL,
+        "prompt" : text
+    })
+    return response.json()["embeddings"]
