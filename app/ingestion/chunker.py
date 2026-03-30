dbc90b7b6b82d2a299a430d0199fdf6bc3e7d5b9
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-29 23:43:26 -0700

    Added chunker logic

--- app/ingestion/chunker.py
+++ app/ingestion/chunker.py
@@ -1,1 +1,13 @@
-#Split text
+# Split text
+
+def chunk_text(text, chunk_size = 300, overlap = 50):
+    chunks = []
+    start = 0
+    
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start : end])
+    # If chunks are completely separate, you can lose context 
+    # So we keep a small overlap between chunks
+        start += chunk_size - overlap
+    return chunks
